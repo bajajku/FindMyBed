3154a92af6b3d661e5df3eb43ba59719e2b957fa
Kunal Bajaj <bajajku@sheridancollege.ca>
2024-11-06 11:44:30 -0500

    updated patient.py and gitignore

--- models/patient.py
+++ models/patient.py
@@ -6,12 +6,12 @@ from utils.constants import ARRIVAL_TIMES
 class Patient:
     patientType: str
     gpsPos: Tuple[float, float]
-    bedType: str = ""
     del24HrPlus: bool
     transportNeedCnt: int
     specialNeedType: str
     specialNeeds: List[str]
     arrival_time: int
+    bedType: str = ""
     assignedHospital: str = ""
     homeHospital: Optional[str] = None
 

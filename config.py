c29519a5d2cca187601f84d292eb075dd217e7a2
doh6077 <kimdohee@sheridancollege.ca>
2024-12-12 11:23:28 -0500

    Update config gile with refined comments and remove arrival time constant (it is in constants file)

--- config.py
+++ config.py
@@ -4,14 +4,13 @@ Use This file to add Global constants.
 '''
 
 NUMBER_OF_DAYS = 365
-ARRIVAL_TIMES = [9, 14, 21]
 
-
-# Add file under data folder and update this constant.
+#Path to the input Excel file containing historical hospital data.
 EXCEL_PATH = 'data/averagePerDay_v5.xlsx'
-
+# Path to the input Excel file containing historical patient data. 
 EXCEL_PATH_NEWDATA = 'data/For Nick-Nov19-FindMyBed-transportData 2021 to 2023-Bed Type-hospital options.xlsx'
 
+# path to the output files 
 REPORT = "output/report.pdf"
 
 TABLE = "output/patients_tables.xlsx"
